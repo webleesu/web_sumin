216f2f289d046764adad4bbd17f8a4aeb37b028d
webleesu <websumin@gmail.com>
2021-11-04 12:51:54 +0900

    array

--- study_code/b_step_03/js/src/b_02_data_type.js
+++ study_code/b_step_03/js/src/b_02_data_type.js
@@ -3,7 +3,7 @@
 // 숫자로 보이는 문자 ('1')를 강제로 숫자형타입으로 변환하는 함수
 
 
-// 숫자(number)
+// 숫자(number)--------------------------------------------------------------------------------------
 var num = 7;
 var num2 = num;
     num = 10;
@@ -11,7 +11,7 @@ var num2 = num;
 console.log( num, typeof(num) );
 console.log( num2, typeof(num2) );
 
-// 문자(string)
+// 문자(string) ---------------------------------------------------------------------------------------
 var str = '123';
 var str2 = str;
     str = '문자'
@@ -41,14 +41,14 @@ console.log( '0~1까지의 난수', parseInt( Math.random() * 10 ) );
 console.log( '절대값', Math.abs(-n) );
 
 
-// 논리(boolean)
+// 논리(boolean) --------------------------------------------------------------------------------------
 // 논리형은 결과를 true/false로 확인하여 처리하지만, 그 결과를 확인하는 검증이 매우 많다.
 // !, === ...
 var bool = false;
 console.log( bool, typeof(bool) );
 
 // 존재하는 값에 !를 붙이면 값이 없다는 의미로 -> false가 되지만 본 의도는 값이 있느냐 없느냐 판단용도로 쓰면 ! 한번더
-.console.log(!n); // false
+console.log(!n); // false
 console.log(!!n); 
 console.log(!!0);
 
@@ -56,8 +56,74 @@ console.log( 1 == '1' ); // true / 유사의 유무
 console.log( 1 === '1' ); // false / 동일의 유무
 
 
-// undefined
-// null
-// Object
-// Array
-// function
+// undefined ------------------------------------------------------------------------------------------
+var und = undefined;
+console.log( und, typeof(und) );
+und = 'undefined';
+console.log( und, typeof(und) );
+und = undefined + 'result!';
+console.log( und, typeof(und) );
+console.log( true + '값' );
+
+
+// null -----------------------------------------------------------------------------------------------
+var nul = null;
+console.log( nul, typeof(nul) );
+
+// null과 undifined는 값이 없다는 의미의 특수한 형타입으로 있던 자료구조였어나, 현재는 단순히 값이 누락시킨다 라는 의미로만 존재
+
+
+
+
+
+// Object/function 형타입은 단순 한가지의 개념을 가지고 있는 것이 아니고 여러 형타입을 담는 복합성의 특징
+// 그렇기에 Object/function 형타입으로 사용할 변수처리는 참조변수 개념으로 처리
+// 일반변수 = 단순 값을 가지는 변수  /  참조변수 = 값을 가지는 변수가 아니라 어느 공간(주소)을 공유하는 변수
+
+// Object ---------------------------------------------------------------------------------------------
+
+// 1. Array ----------------------------------------------------------------------------------------------
+var arr = ['키보드', '마우스', '태블릿', '모니터', '노트북', '웹캠'];
+console.log( arr.constructor );
+console.log( typeof(arr) );
+console.log( arr[3] ); // 모니터
+
+arr[5] = '마이크'; // 웹캠 -> 마이크
+arr[9] = '웹캠';
+console.log( arr[5], arr[9] );
+console.log( arr[5], arr );
+
+var arr2 = arr;
+console.log( arr2 );
+arr[9] = '빼빼로';
+console.log( arr );
+console.log( arr2 );
+
+console.clear();
+
+var arT = new Array();
+console.log( arT );
+arT[0] = '물';
+arT[1] = '약';
+console.log( arT );
+// new Array()를 이용하여 배열을 생성하는 방식은 권장하지 않는다. -> 바로 [] 작성 후 바로 수행
+
+var arA = [];
+arA[0] = '물';
+arA[1] = '약';
+
+arA.push('커피');
+arA.push('쥬스');
+arA.push('밥');
+arA.unshift(5);
+arA.unshift(0);
+arA.unshift(0000);
+
+console.log( arA );
+
+arA.pop();
+arA.shift();
+console.log( arA );
+
+
+// function -------------------------------------------------------------------------------------------
