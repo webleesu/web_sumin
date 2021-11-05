8b59f8560f35044452f4c73e9feec6667131f3e5
webleesu <websumin@gmail.com>
2021-11-05 18:15:20 +0900

    js 코드 추가

--- study_code/b_step_03/js/src/b_02_js_data_type_summary2.js
+++ study_code/b_step_03/js/src/b_02_js_data_type_summary2.js
@@ -1,1 +1,29 @@
 // b_02_js_data_type_summary2.js
+
+console.clear();
+var dicList = [];
+// dicList내부에 계산한 값을 히스토리처럼 저장!!
+
+/*
+var dicList = [];
+dicList.push('a');
+dicList.push('b');
+dicList.push('c');
+dicList.push('d');
+dicList.push('e');
+dicList.push('f');
+
+console.log( dicList );
+*/
+
+var fn1 = function( data ){
+  return dicList.push( data );
+};
+
+fn1('button');
+fn1('link');
+fn1('anker');
+fn1('div');
+fn1('section');
+
+console.log( dicList );
