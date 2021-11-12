9ffe3cc29e69332510a30b2ca0a812a8ebae158c
webleesu <websumin@gmail.com>
2021-11-12 12:06:11 +0900

    생성자 함수 사용, 요소 생성

--- study_code/b_step_03/js/src/b_08_js_repeat1.js
+++ study_code/b_step_03/js/src/b_08_js_repeat1.js
@@ -36,3 +36,40 @@ while(w < 20){
 for( var y = 0; y<20; y+=2 ){
   console.log('for: ', y);
 }
+
+console.clear();
+
+/*
+for(var year = 2021; year > 1970; year -= 1){
+  console.log(year+'년');
+  document.write(year);
+}
+*/
+
+// 생성자 함수를 사용하는 방법
+// 1. 첫글자가 대문자
+// 2. 생성자 그대로 사용하는 것이 아닌 해당하는 함수를 복제하여 객체로 사용/ 'new'연산자를 붙여서 사용
+// 3. 함수 중에 기존에 존재하는 함수를 내장함수라고 불리우며, 생성자 함수도 내장되어있는 기능이 있다.
+
+// 날짜와 관련된 함수 Date();
+
+var date = new Date();
+console.log( date.getFullYear() );
+
+var yy = date.getFullYear();
+var text = '년';
+var targetY = yy - 50;
+var ul = document.querySelector('.test');
+var li;
+
+for(; yy >= targetY; yy -= 1){
+  // console.log( yy + text );
+  li = document.createElement('li');
+  li.innerText = yy + text;
+  ul.append(li);
+}
+
+// 요소를 생성하는 방법
+// 1. document.creat(요소이름);
+// 2. 생성된 요소에 기능 첨부
+// 3. 생성된 요소를 선택된 요소에 담기 : append()
