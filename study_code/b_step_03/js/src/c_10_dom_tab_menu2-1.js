9265a557dac2300a09c010f5e3bf9f75b0d26461
webleesu <websumin@gmail.com>
2021-12-02 12:00:48 +0900

    [add] : javascript - 객체형식 내용을 생성된 li에 첨부

--- study_code/b_step_03/js/src/c_10_dom_tab_menu2-1.js
+++ study_code/b_step_03/js/src/c_10_dom_tab_menu2-1.js
@@ -1,30 +1,62 @@
 // c_10_dom_tab_menu2-1.js
 
-var innerCode = '<a href="#" data-id="event_21042"><h4 class="event_title">summer flaver</h4><p class="evetn_narration">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat, beatae.</p><dl class="date"><dt class="blind">기간</dt><dd>2122. 01. 05 - 2022. 02. 15</dd></dl><dl class="event_check success"><dt>이벤트 진행 상태</dt><dd>이벤트 종료</dd></dl></a>';
+// * li요소에 삽입할 객체 리스트
+var data = [
+  {id: 'event_21042', title: 'summer flaver'}
+];
 
 
-// 1. html 요소를 생성, 지정된 위치에 삽입 - innerHTML, append
-
+/*
+  1. html 요소를 생성, 지정된 위치에 삽입 - innerHTML, append, creatElement
+  2. 한번에 생성+첨부를 반복할 수 없기에 생성+첨부를 하나의 세트로 인식하여 반복수행
+  3. 별도의 data내용을 기반으로 해당하는 자료에 맞게 각각 첨부
+*/
 // 선택변수
 var contentInner = document.querySelector('.content_inner');
 var yearPart = contentInner.querySelectorAll('.year_part');
 var latestYear = yearPart[0];
 
 // 기능
-  // 1. ul 생성
+// 0. 삽입할 요소
+var innerCode = '<a href="#" data-id><h4 class="event_title">제목</h4><p class="event_narration">설명</p><dl class="date"><dt class="blind">기간</dt><dd>날짜</dd></dl><dl class="event_check success"><dt></dt><dd>진행예정</dd></dl></a>';
+// 1. ul 생성
   var makeUl = document.createElement('ul');
-  var makeLi = document.createElement('li');
-
-
+  makeUl.setAttribute('class', 'event_particle');
+  latestYear.append( makeUl );
+  /*
   makeUl.className = 'two'; // 기존내용지우고 대체
   makeUl.classList.add('one');// 기존에 추가
   makeUl.setAttribute('data-id','three'); // 대체, 속성 변경가능
+  */
 
-  latestYear.append( makeUl );
+  
   // latestYear.appendChild( mkt );// 문자불가능
   var yearUl = latestYear.querySelector('ul');
-  yearUl.append(makeLi);
+  // var makeLi = document.createElement('li');
+  // yearUl.append(makeLi);
   
+  // 2. li 5개 생성하여 첨부
+  var i=0;
+  var makeLiLen = data.length; // 실제 생성할 배열의 갯수만큼 처리
+  var makeLi;//  = document.createElement('li');
+
+  for(; i < makeLiLen; i+=1){
+    makeLi = document.createElement('li');
+    makeLi.innerHTML = innerCode;
+    yearUl.append(makeLi);
+  }
+
+  // 생성한 내용 기준 li에 내용 첨부
+  var selectI = 0;
+  var eventList = yearUl.querySelectorAll('li');
+  var selectorLi = eventList[selectI];
+  var selectorLink = selectorLi.querySelector('h4');
+  var selectorH4 = selectorLi.querySelector('h4');
+
+  // selectorH4 = data[selectI].title;
+
+  selectorLink.setAttribute('data-id', data[selectI].id);
+  selectorH4.innerText = data[selectI].title;
 
   
 // -----------------------------------------------------------------------------------------------------
