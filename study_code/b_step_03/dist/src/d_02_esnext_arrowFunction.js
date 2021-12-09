346b2e1034eab53473033138a694158e9380838e
webleesu <websumin@gmail.com>
2021-12-09 14:34:50 +0900

    [add] esnext - 생성자함수를 class로 변환 기본1

--- study_code/b_step_03/dist/src/d_02_esnext_arrowFunction.js
+++ study_code/b_step_03/dist/src/d_02_esnext_arrowFunction.js
@@ -36,11 +36,57 @@ const arrNum = [1,2,4,6,8,9,20,40,56,75,88];
 // const minNum = arrNum.filter(function(part){
 //   return 40 >= part && part > 10;
 // });
-
-
-const minNum = arrNum.filter( part => 40 >= part && part > 10 );
+  
+// const minNum = arrNum.filter( part => 40 >= part && part > 10 );
+const minNum = arrNum.filter( (part) => {
+  return 40 >= part && part > 10
+});
 console.log(minNum);
+  
+// ------------------------------------------
 
-// 즉시실행함수
+const TimeTable = function(name, kor, eng, math){
+  this.name = name;
+  this.korean = kor;
+  this.english = eng;
+  this.math = math;
+};
 
-// 함수 선언식
+TimeTable.prototype.subject = '2021년 학습 과정명';
+
+const data = [];
+
+const st1 = new TimeTable('leesu', 70, 80, 90);
+data.push(st1);
+console.log(data);
+
+// ------------------------------------------
+// 생성자 함수로는 화살표 함수를 사용할 수 없다.
+// this는 매개변수로 인식되어 실제로 this의 개념이 아닌것으로 판단.
+// data 자체에 값이 없으면 에러,
+// 생성자로 사용시 타입이 변환 불가능이라 에러
+// prototype에 값이 없음
+
+const FnData = () => {}; 
+const myFn = new FnData();
+
+// ------------------------------------------
+// 생성자 함수는 과거의 형태 그대로 유지
+// 타 언어에서는 이러한 개념을 class라고 불리운다.
+// 그래서 js에서도 class라는 명칭을 사용
+
+// const PersonResult = function(name, kor, eng){
+//  this.name = name;
+//  this.kor = kor;
+//  this.eng = eng;
+// }
+
+class PersonResult {
+  constructor (name, kor, eng){
+    this.name = name;
+    this.kor = kor;
+    this.eng = eng;
+  }
+}; 
+const person = new PersonResult('x', 70, 90);
+console.log(person);
