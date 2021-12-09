8a343e7ccdf981a87437c47c03cf4358fbc89eb0
webleesu <websumin@gmail.com>
2021-12-09 12:23:25 +0900

    [add] javascript(arrow function) - esnext - 화살표함수2

---
+++ study_code/b_step_03/dist/src/d_02_esnext_arrowFunction.js
@@ -0,0 +1,46 @@
+// d_02_esnext_arrowFunction.js
+// 화살표 함수는 생성자 함수 또는 return처리할 값이 없는 함수를 제외하고 사용이 가능
+
+// 함수표현식
+const fnBox = function(a){
+  return a * 10;
+};
+
+fnBox(5);
+// ------------------------------------------
+const fnBox2 = (a) => a * 10;
+
+// console.log( fnBox2 );
+// ------------------------------------------
+
+// 함수 선언식
+function fnBox3(a, b){
+  const c = (a + b) * 100 + 'size';
+  return c;
+}
+
+let rel = fnBox3(5,10);
+console.log(rel);
+
+const fnBox4 = (a, b) => {
+  const c = (a + b) * 100 + 'size';
+  return c;
+}
+
+let rel2 = fnBox4(10,3);
+console.log(rel2);
+
+// ------------------------------------------
+
+const arrNum = [1,2,4,6,8,9,20,40,56,75,88];
+// const minNum = arrNum.filter(function(part){
+//   return 40 >= part && part > 10;
+// });
+
+
+const minNum = arrNum.filter( part => 40 >= part && part > 10 );
+console.log(minNum);
+
+// 즉시실행함수
+
+// 함수 선언식
---
+++ study_code/b_step_03/js/src/d_02_esnext_arrowFunction.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// d_02_esnext_arrowFunction.js
+// 화살표 함수는 생성자 함수 또는 return처리할 값이 없는 함수를 제외하고 사용이 가능
+// 함수표현식
+var fnBox = function fnBox(a) {
+  return a * 10;
+};
+
+fnBox(5); // ------------------------------------------
+
+var fnBox2 = function fnBox2(a) {
+  return a * 10;
+}; // console.log( fnBox2 );
+// ------------------------------------------
+// 함수 선언식
+
+
+function fnBox3(a, b) {
+  var c = (a + b) * 100 + 'size';
+  return c;
+}
+
+var rel = fnBox3(5, 10);
+console.log(rel);
+
+var fnBox4 = function fnBox4(a, b) {
+  var c = (a + b) * 100 + 'size';
+  return c;
+};
+
+var rel2 = fnBox4(10, 3);
+console.log(rel2); // ------------------------------------------
+
+var arrNum = [1, 2, 4, 6, 8, 9, 20, 40, 56, 75, 88]; // const minNum = arrNum.filter(function(part){
+//   return 40 >= part && part > 10;
+// });
+
+var minNum = arrNum.filter(function (part) {
+  return 40 >= part && part > 10;
+});
+console.log(minNum); // 즉시실행함수
+// 함수 선언식
