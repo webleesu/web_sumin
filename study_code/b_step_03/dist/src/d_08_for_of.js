a175b097181f96dbae9af2658d01a5b411a66bde
webleesu <websumin@gmail.com>
2021-12-13 15:20:44 +0900

    [add] for-of - javascript for-of

--- study_code/b_step_03/dist/src/d_08_for_of.js
+++ study_code/b_step_03/dist/src/d_08_for_of.js
@@ -14,3 +14,30 @@ arr.forEach((data)=> { console.log(data)} );
 for(let i of arr){
   console.log(i);
 }
+
+// --------------------------------
+let text = 'javascript';
+
+for(let i of text){
+  console.log(i);
+}
+
+const obj = {
+  'samsung': 'galaxy',
+  'apple'  : 'iphone',
+  'lg'     : 'wing',
+  'nokia'  : 'N serize'
+};
+
+// for(var i in obj){
+//   console.log(i);
+// }
+
+const key = Object.keys(obj);
+const val = Object.values(obj);
+const ent = Object.entries(obj);
+console.log(key, val);
+console.log(ent);
+for(let [i,v] of ent){
+  console.log(`${i} : ${v}`);
+}
