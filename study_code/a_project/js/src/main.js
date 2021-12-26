6e10166e7fea7ee51769f1ac30dc760f8e31160a
webleesu <websumin@gmail.com>
2021-12-27 02:15:28 +0900

    [add] js 기능 추가

---
+++ study_code/a_project/js/src/main.js
@@ -0,0 +1,118 @@
+// main.js
+
+// #header 언어부분 ======================================================================
+// 변수 ------------------------------------------------------------------
+const elLanguage = document.querySelector('.unb_language');
+const elLanArea = elLanguage.querySelector('.unb_lauguage_area');
+let clickCount = 0;
+
+elLanguage.addEventListener('click', (e)=>{
+  e.preventDefault();
+  
+
+  if(clickCount === 0){
+    elLanArea.style.display = 'block';
+    clickCount = 1;
+  }else {
+    elLanArea.style.display = 'none';
+    clickCount = 0;
+  }
+})
+
+
+
+
+// #viewBox 광고영역 슬라이드 ==============================================================
+// 변수 ------------------------------------------------------------------
+const elViewBox = document.querySelector('#viewBox');
+const elViewArea = elViewBox.querySelector('.view_area');
+
+const elViewBtn = elViewBox.querySelector('.view_btn');
+const elNext = elViewBox.querySelector('.view_after_btn');;
+const elPrev = elViewBox.querySelector('.view_before_btn');;
+
+const view = elViewArea.querySelector('.view');
+const conUl = view.querySelector('.view_in_wrap');
+const conLi = conUl.querySelectorAll('.view_content');
+
+const elTotalCount = elViewArea.querySelector('.view_total_count');
+const elNowCount = elViewArea.querySelector('.view_now_count');
+// -----------------------------------------------------------------------
+// 추가 변수
+let SLIDE_COUNT = 0;
+const slideLen = conLi.length;
+let BEFORE_COUNT = SLIDE_COUNT;
+let timed = 500;
+let cssFn = 'ease';
+let permission = true;
+
+// -----------------------------------------------------------------------
+
+elTotalCount.innerText = slideLen; // 인디케이터 숫자 토탈숫자 넣기
+// 현재 슬라이드 위치 표현
+const fnNowCount = () => {
+  elNowCount.innerText = SLIDE_COUNT + 1;
+}
+
+// conLi[SLIDE_COUNT].classList.add('on');
+
+const fnDelay = async ( t = timed ) => {
+  const promise = new Promise(resolve => {
+    setTimeout( ()=> {
+      permission = true;
+      resolve(permission);
+    }, t)
+  });
+  return await promise;
+};
+
+const fnOpacity = () => {
+  conLi[SLIDE_COUNT].style.display = 'block';
+  // conLi[SLIDE_COUNT].style.display = 'block';
+
+  conLi[BEFORE_COUNT].style.transition = `all ${timed}ms ease`;
+  conLi[BEFORE_COUNT].style.opacity = 0;
+
+  conLi[BEFORE_COUNT].classList.remove('on');
+  conLi[SLIDE_COUNT].classList.add('on');
+
+  fnDelay(timed)
+    .then( () => {
+      conLi[BEFORE_COUNT].removeAttribute('style');
+      conLi[BEFORE_COUNT].classList.remove('on');
+      conLi[SLIDE_COUNT].classList.add('on');
+      BEFORE_COUNT = SLIDE_COUNT;
+    })
+}
+
+// elNext
+elNext.addEventListener('click', (e)=>{
+  e.preventDefault();
+  
+  if(permission){
+    permission = false;
+    if(SLIDE_COUNT < slideLen-1){
+      SLIDE_COUNT += 1;
+    }else {
+      SLIDE_COUNT = 0;
+    }
+    fnOpacity();
+    fnNowCount();
+  }
+});
+
+// elPrev
+elPrev.addEventListener('click', (e)=>{
+  e.preventDefault();
+
+  if(permission){
+    permission = false;
+    if(SLIDE_COUNT <= 0){
+      SLIDE_COUNT = slideLen -1;
+    }else{
+      SLIDE_COUNT -= 1;
+    }
+    fnOpacity();
+    fnNowCount();
+  }
+});
