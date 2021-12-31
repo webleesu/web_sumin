5c2bcd34460dda838d1c3b279b7c599353a78f3d
webleesu <websumin@gmail.com>
2021-12-31 12:15:16 +0900

    [add]  form영역에서 비밀번호확인기능 처리

--- study_code/b_step_03/dist/src/d_13_class2.js
+++ study_code/b_step_03/dist/src/d_13_class2.js
@@ -19,19 +19,16 @@ class CheckMember {
 // --------------------------------------------------------------------
 // 이벤트
 // 비밀번호 확인버튼 클릭시 비번 확인
-pwViewBtn.addEventListener('mousedown', (e)=>{
-  // console.log( e.button ); // 왼버튼 0, 휠버튼 1, 오른버튼 2
-  e.preventDefault();
-  // 왼버튼 클릭 확인 (e.button에서 0)
-  if(e.button === 0){ 
-    userPw.type = 'text'; // 왼버튼 누르면 입력한 pw 보여지게
-  }
-});
-
 const fnPwType = function(e){
   e.preventDefault();
-  userPw.type = 'password'; // 왼버튼 떼면 다시 비밀번호 가려지게
-}
+  if( e.button === 0 && e.type === 'mousedown'){ // 왼버튼 클릭 확인 (e.button에서 0)
+    userPw.type = 'text'; // 왼버튼 누르면 입력한 pw 보여지게
+  }else {
+    userPw.type = 'password'; // 왼버튼 떼면 다시 비밀번호 가려지게
+  }
+};
+
+pwViewBtn.addEventListener('mousedown', fnPwType );
 
 pwViewBtn.addEventListener('mouseup',  fnPwType );
 
