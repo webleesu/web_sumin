b2e6c0030fe108d991b119d805754ed257880303
webleesu <websumin@gmail.com>
2022-01-05 17:40:57 +0900

    [modify]  카드 리스트 체크에 대한 내용 수정

--- study_code/b_step_06/src/App.js
+++ study_code/b_step_06/src/App.js
@@ -3,12 +3,41 @@ import Header from './component/Header';
 import Main from './component/Main';
 import Footer from './component/Footer';
 import './App.css';
+import { useState } from 'react/cjs/react.development';
 
 function App() {
   const title = 'site';
 
+  const style={
+    backgroundColor: "#035", 
+    color: "#fff", 
+    padding: "1rem",
+    border: "1px solid #333"
+  };
+
+  // let par = '내용 변경 전';
+  const dataArr = [
+    '내용 변경 전', 
+    '값이 변경되었습니다.',
+    '추가 변경되었습니다.',
+    '또 바뀌었습니다.'
+  ];
+  let i=0;
+  const [par, setPar] = useState(dataArr[i]);
+  const pChange = (e) => {
+    e.preventDefault();
+    // const p = document.querySelector('p');
+    setPar(dataArr[i+=1])
+
+  }
+
+  
   return (
     <div className="App">
+      <button style={style} onClick={pChange}>클릭</button>
+      <p>{par}</p>
+
+
       <Header heading={title} />
       <Main />    
       <Footer heading={title} />
