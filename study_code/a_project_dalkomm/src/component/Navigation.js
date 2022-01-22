31aebf79e043c04f54cba2b6c2f5fac177993afa
webleesu <websumin@gmail.com>
2022-01-22 12:36:14 +0900

    [add] navigation 클릭시 underline 생성

--- study_code/a_project_dalkomm/src/component/Navigation.js
+++ study_code/a_project_dalkomm/src/component/Navigation.js
@@ -2,14 +2,30 @@
 import '../style/navigation.scss';
 
 export default function Navigation(){
+  const elUnderline = document.querySelector('.underline');
+  const elNavigationLi = document.querySelectorAll('.gnb li'); // li를 선택하게 한 후 이벤트 캡쳐 이용
+
+  const fnUnderlineClick = (e) => {
+    e.preventDefault();
+    elUnderline.style.width = e.currentTarget.offsetWidth + 'px';
+    elUnderline.style.left = e.currentTarget.offsetLeft + 'px';
+    elUnderline.style.top = e.currentTarget.offsetTop + e.currentTarget.offsetHeight + 'px';
+
+    // console.log(elUnderline.style.width);
+    // console.log(e.currentTarget);
+  }
+
+  elNavigationLi.forEach(li => li.addEventListener('click', (e)=>fnUnderlineClick));
+  
   return(
     <nav className="gnb">
+        <div className='underline'></div>
       <ul>
-        <li><a href="#">Dal.Komm</a></li>
-        <li><a href="#">메뉴 · 매장</a></li>
-        <li><a href="#">창업안내</a></li>
-        <li><a href="#">새로운 소식</a></li>
-        <li><a href="#">SHOP</a></li>
+        <li><a href="#"  onClick={fnUnderlineClick}>Dal.Komm</a></li>
+        <li><a href="#"  onClick={fnUnderlineClick}>메뉴 · 매장</a></li>
+        <li><a href="#"  onClick={fnUnderlineClick}>창업안내</a></li>
+        <li><a href="#"  onClick={fnUnderlineClick}>새로운 소식</a></li>
+        <li><a href="#"  onClick={fnUnderlineClick}>SHOP</a></li>
       </ul>
     </nav>
   );
