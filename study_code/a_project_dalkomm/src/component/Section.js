649afda70b596781013ee08b6f008ae43a21a0cc
webleesu <websumin@gmail.com>
2022-01-27 22:25:06 +0900

    [add] view_07

--- study_code/a_project_dalkomm/src/component/Section.js
+++ study_code/a_project_dalkomm/src/component/Section.js
@@ -40,26 +40,43 @@ export default function Section() {
           </li>
 
           <li className='view_03'>
-            <h2 className='elice'>
-              <strong>
-                환상의 바리스타&nbsp;
-                <img src={Dal_K} alt='Dal.K'/>
-                와 얼렁뚱땅 파티쉐&nbsp;
-                <img src={Fox_M} alt='Fox.M' />
-              </strong>
-            </h2>
-            <p>
-              그들의 이야기로 펼쳐지는
-              <br />
-              달콤의 상상력을 기대해주세요
-            </p>
+            <section className="view_03_text_wrap">
+              <h2 className='elice'>
+                <strong>
+                  환상의 바리스타&nbsp;
+                  <img src={Dal_K} alt='Dal.K'/>
+                  와 얼렁뚱땅 파티쉐&nbsp;
+                  <img src={Fox_M} alt='Fox.M' />
+                </strong>
+              </h2>
+              <p>
+                그들의 이야기로 펼쳐지는
+                <br />
+                달콤의 상상력을 기대해주세요
+              </p>
+            </section>
             <div className='view_03_bg'></div>
           </li>
 
           <li className='view_04'></li>
           <li className='view_05'></li>
           <li className='view_06'></li>
-          <li className='view_07'></li>
+
+          <li className='view_07'>
+            <section className='view_07_text_wrap'>
+              <h2 className='elice'>
+                카페 창업의 꿈,
+                <br />
+                달콤이 서포터즈가 되겠습니다
+              </h2>
+              <p>
+                달콤은 파트너 여러분들의 다양한 창업 조건에 맞는 '맞춤형 점포 개설'을 통해
+                <br />
+                카페 창업의 문턱을 낮추고 진정한 상생의 가치를 구현합니다.
+              </p>
+            </section>
+            <button className='franchisee' href='#'>가맹점 개설 안내</button>
+          </li>
         </ul>
       </section>
   );
